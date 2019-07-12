81a3569f2f46bf5360cecdce5990e97c88b9b440
hide <razyetts@hotmail.co.jp>
2019-07-13 00:55:09 +0900

    exportsとrequireの組み合わせが悪くてエラー起きてた？

--- nuxt/plugins/contentful.js
+++ nuxt/plugins/contentful.js
@@ -2,13 +2,11 @@ const contentful = require('contentful')
 // use default environment config for convenience
 // these will be set via `env` property in nuxt.config.js
 const config = {
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+  space: process.env.ctfSpaceId,
+  accessToken: process.env.ctfCdaAccessToken
 }
 
 // export `createClient` to use it in page components
-module.exports = {
-  createClient () {
-    return contentful.createClient(config)
-  }
+export function createClient() {
+  return contentful.createClient(config)
 }
