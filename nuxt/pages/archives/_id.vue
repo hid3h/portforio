779e57e5225187b816d83e3df11781cc2154c105
hide <razyetts@hotmail.co.jp>
2019-07-13 16:58:35 +0900

    個別ページ用意

---
+++ nuxt/pages/archives/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ archive.fields.title }}</h1>
+    <div>{{ archive.fields.body }}</div>
+  </div>
+</template>
+
+<script>
+import { createClient } from '~/plugins/contentful.js'
+
+const client = createClient()
+
+export default {
+  async asyncData({ env, params }) {
+      console.log(params.id)
+    const archive = await client.getEntry("1w86cH51mRtlT9oTs26sLg");
+    return {
+      archive: archive
+    }
+  }
+}
+</script>
+
+<style>
+</style>
